600f76f2685bd0680b9ac27461816795632ac5e0
Tanmay Kulkarni <iamtanmaykulkarni@gmail.com>
2017-08-08 01:23:58 +0530

    Changes suggested by Ketaki

--- thank_you/js/index.js
+++ thank_you/js/index.js
@@ -45,13 +45,13 @@
   });
 
   quotes = _([
-    {
-      text: 'When I hear somebody sigh, "Life is Hard," I am always tempted to ask, "Compared to what?"',
-        author: 'Sydney Harris'
-    },
+    // {
+    //   text: 'When I hear somebody sigh, "Life is Hard," I am always tempted to ask, "Compared to what?"',
+    //     author: 'Sydney Harris'
+    // },
     {
       text: 'Thank You!',
-      author: 'A research by Ketaki Diwan.'
+      author: ''
     }
     // {
     //   text: 'Life isn’t about getting and having, it’s about giving and being.',
